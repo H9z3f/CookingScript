6838871036780d7f9dba2fdaa635fd662e47c6ed
George <georgy_minakov@mail.ru>
2022-09-25 14:37:29 +0300

    bugfix v.1

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ from PIL import Image
 import requests as req
 
 
-def rSalad(defPos):
+def rSalad(defPos, imgData):
     while True:
-        mPos = los(defPos["meat"], confidence=0.7)
-        ePos = los(defPos["eggs"], confidence=0.7)
-        vPos = los(defPos["vegetable"], confidence=0.7)
+        mPos = los(imgData["meat"], confidence=0.7)
+        ePos = los(imgData["eggs"], confidence=0.7)
+        vPos = los(imgData["vegetable"], confidence=0.7)
         if mPos and ePos and vPos:
             mt(mPos[0], mPos[1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             mt(ePos[0], ePos[1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             mt(vPos[0], vPos[1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             mt(defPos["knife"][0], defPos["knife"][1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             mt(defPos["fire"][0], defPos["fire"][1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             pag.leftClick(x=defPos["start"][0], y=defPos["start"][1])
             sleep(5)
         else:
@@ -28,19 +28,19 @@ def rSalad(defPos):
             main()
 
 
-def stew(defPos):
+def stew(defPos, imgData):
     while True:
-        mPos = los(defPos["meat"], confidence=0.7)
-        vPos = los(defPos["vegetable"], confidence=0.7)
+        mPos = los(imgData["meat"], confidence=0.7)
+        vPos = los(imgData["vegetable"], confidence=0.7)
         if mPos and vPos:
             mt(mPos[0], mPos[1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             mt(vPos[0], vPos[1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             mt(defPos["water"][0], defPos["water"][1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             mt(defPos["fire"][0], defPos["fire"][1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             pag.leftClick(x=defPos["start"][0], y=defPos["start"][1])
             sleep(5)
         else:
@@ -48,16 +48,16 @@ def stew(defPos):
             main()
 
 
-def vSmoothie(defPos):
+def vSmoothie(defPos, imgData):
     while True:
-        vPos = los(defPos["vegetable"], confidence=0.7)
+        vPos = los(imgData["vegetable"], confidence=0.7)
         if vPos:
             mt(vPos[0], vPos[1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             mt(defPos["water"][0], defPos["water"][1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             mt(defPos["whisk"][0], defPos["whisk"][1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             pag.leftClick(x=defPos["start"][0], y=defPos["start"][1])
             sleep(5)
         else:
@@ -65,16 +65,16 @@ def vSmoothie(defPos):
             main()
 
 
-def fSmoothie(defPos):
+def fSmoothie(defPos, imgData):
     while True:
-        fPos = los(defPos["fruit"], confidence=0.7)
+        fPos = los(imgData["fruit"], confidence=0.7)
         if fPos:
             mt(fPos[0], fPos[1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             mt(defPos["water"][0], defPos["water"][1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             mt(defPos["whisk"][0], defPos["whisk"][1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             pag.leftClick(x=defPos["start"][0], y=defPos["start"][1])
             sleep(5)
         else:
@@ -82,14 +82,14 @@ def fSmoothie(defPos):
             main()
 
 
-def vSalad(defPos):
+def vSalad(defPos, imgData):
     while True:
-        vPos = los(defPos["vegetable"], confidence=0.7)
+        vPos = los(imgData["vegetable"], confidence=0.7)
         if vPos:
             mt(vPos[0], vPos[1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             mt(defPos["knife"][0], defPos["knife"][1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             pag.leftClick(x=defPos["start"][0], y=defPos["start"][1])
             sleep(5)
         else:
@@ -97,14 +97,14 @@ def vSalad(defPos):
             main()
 
 
-def fSalad(defPos):
+def fSalad(defPos, imgData):
     while True:
-        fPos = los(defPos["fruit"], confidence=0.7)
+        fPos = los(imgData["fruit"], confidence=0.7)
         if fPos:
             mt(fPos[0], fPos[1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             mt(defPos["knife"][0], defPos["knife"][1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             pag.leftClick(x=defPos["start"][0], y=defPos["start"][1])
             sleep(5)
         else:
@@ -112,14 +112,14 @@ def fSalad(defPos):
             main()
 
 
-def sEggs(defPos):
+def sEggs(defPos, imgData):
     while True:
-        ePos = los(defPos["eggs"], confidence=0.7)
+        ePos = los(imgData["eggs"], confidence=0.7)
         if ePos:
             mt(ePos[0], ePos[1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             mt(defPos["fire"][0], defPos["fire"][1])
-            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.5)
+            dt(defPos["knife"][0], defPos["knife"][1] - 150, 0.25)
             pag.leftClick(x=defPos["start"][0], y=defPos["start"][1])
             sleep(5)
         else:
@@ -139,62 +139,69 @@ def main():
         if kb.is_pressed("e"):
             sleep(0.5)
             defPos = {
-                "knife": los(Image.open(req.get(
-                    "https://github.com/H9z3f/CookingScript/blob/main/images/knife.png?raw=true",
-                    stream=True
-                ).raw), confidence=0.7),
-                "whisk": los(Image.open(req.get(
-                    "https://github.com/H9z3f/CookingScript/blob/main/images/whisk.png?raw=true",
-                    stream=True
-                ).raw), confidence=0.7),
-                "fire": los(Image.open(req.get(
-                    "https://github.com/H9z3f/CookingScript/blob/main/images/fire.png?raw=true",
-                    stream=True
-                ).raw), confidence=0.7),
-                "water": los(Image.open(req.get(
-                    "https://github.com/H9z3f/CookingScript/blob/main/images/water.png?raw=true",
-                    stream=True
-                ).raw), confidence=0.7),
-                "start": los(Image.open(req.get(
-                    "https://github.com/H9z3f/CookingScript/blob/main/images/start.png?raw=true",
-                    stream=True
-                ).raw), confidence=0.7),
-                "meat": Image.open(req.get(
-                    "https://github.com/H9z3f/CookingScript/blob/main/images/meat.png?raw=true",
-                    stream=True
-                ).raw),
-                "eggs": Image.open(req.get(
-                    "https://github.com/H9z3f/CookingScript/blob/main/images/eggs.png?raw=true",
-                    stream=True
-                ).raw),
-                "vegetable": Image.open(req.get(
-                    "https://github.com/H9z3f/CookingScript/blob/main/images/vegetable.png?raw=true",
-                    stream=True
-                ).raw),
-                "fruit": Image.open(req.get(
-                    "https://github.com/H9z3f/CookingScript/blob/main/images/fruit.png?raw=true",
-                    stream=True
-                ).raw)
+                "knife": los(data["knife"], confidence=0.7),
+                "whisk": los(data["whisk"], confidence=0.7),
+                "fire": los(data["fire"], confidence=0.7),
+                "water": los(data["water"], confidence=0.7),
+                "start": los(data["start"], confidence=0.7)
             }
             match dish:
                 case "Stew[3]":
-                    stew(defPos)
+                    stew(defPos, data)
                 case "Vegetable smoothie[2]":
-                    vSmoothie(defPos)
+                    vSmoothie(defPos, data)
                 case "Fruit smoothie[2]":
-                    fSmoothie(defPos)
+                    fSmoothie(defPos, data)
                 case "Russian salad[5]":
-                    rSalad(defPos)
+                    rSalad(defPos, data)
                 case "Vegetable salad":
-                    vSalad(defPos)
+                    vSalad(defPos, data)
                 case "Fruit salad":
-                    fSalad(defPos)
+                    fSalad(defPos, data)
                 case "Scrambled eggs":
-                    sEggs(defPos)
+                    sEggs(defPos, data)
 
 
-los = pag.locateOnScreen
-mt = pag.moveTo
-dt = pag.dragTo
 if __name__ == "__main__":
+    los = pag.locateOnScreen
+    mt = pag.moveTo
+    dt = pag.dragTo
+    data = {
+        "knife": Image.open(req.get(
+            "https://github.com/H9z3f/CookingScript/blob/main/images/knife.png?raw=true",
+            stream=True
+        ).raw),
+        "whisk": Image.open(req.get(
+            "https://github.com/H9z3f/CookingScript/blob/main/images/whisk.png?raw=true",
+            stream=True
+        ).raw),
+        "fire": Image.open(req.get(
+            "https://github.com/H9z3f/CookingScript/blob/main/images/fire.png?raw=true",
+            stream=True
+        ).raw),
+        "water": Image.open(req.get(
+            "https://github.com/H9z3f/CookingScript/blob/main/images/water.png?raw=true",
+            stream=True
+        ).raw),
+        "start": Image.open(req.get(
+            "https://github.com/H9z3f/CookingScript/blob/main/images/start.png?raw=true",
+            stream=True
+        ).raw),
+        "meat": Image.open(req.get(
+            "https://github.com/H9z3f/CookingScript/blob/main/images/meat.png?raw=true",
+            stream=True
+        ).raw),
+        "eggs": Image.open(req.get(
+            "https://github.com/H9z3f/CookingScript/blob/main/images/eggs.png?raw=true",
+            stream=True
+        ).raw),
+        "vegetable": Image.open(req.get(
+            "https://github.com/H9z3f/CookingScript/blob/main/images/vegetable.png?raw=true",
+            stream=True
+        ).raw),
+        "fruit": Image.open(req.get(
+            "https://github.com/H9z3f/CookingScript/blob/main/images/fruit.png?raw=true",
+            stream=True
+        ).raw)
+    }
     main()
